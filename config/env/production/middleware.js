954fbab9a799c9f1d8d405e6f871acbc1a840dc6
Chris O'Donnell <dev@codfish.io>
2020-12-27 23:59:17 -0500

    fix: add cors setting back with admin url as origin

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,10 +1,14 @@
 module.exports = ({ env }) => ({
   settings: {
     // logger: {
-    //   level: "debug",
+    //   level: 'error',
     // },
-    // cors: {
-    //   origin: ['https://www.katebromley.com', env('PREVIEW_URL')],
-    // },
+    cors: {
+      origin: [
+        'https://www.katebromley.com',
+        env('PREVIEW_URL'),
+        env('RENDER_EXTERNAL_URL')
+      ],
+    },
   },
 });
